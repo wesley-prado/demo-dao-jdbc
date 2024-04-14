9279f3ee5197426579007aefd60ac02e0a5af524
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2024-04-14 19:36:14 -0300

    feat: create Seller entity class ✨

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,26 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(new Department(1, "Books"));
-    }
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
+
+		try {
+			System.out.println( new Seller(
+					1,
+					"Wesley Prado",
+					"wesleyprado.dev@codemages.com",
+					sdf.parse( "01/07/1995" ),
+					10230.0,
+					new Department(1, "Software Engineering")
+			) );
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
 }

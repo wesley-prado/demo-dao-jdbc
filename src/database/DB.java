f38e94f218c2c75d051f552889727e61d9d5bbc8
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2024-04-14 16:59:39 -0300

    chore: initial project settings ✨

---
+++ src/database/DB.java
@@ -0,0 +1,89 @@
+package database;
+
+import java.sql.Statement;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+    private static Connection conn = null;
+
+    private DB() {
+    }
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            Properties props = loadProperties();
+            String url = props.getProperty("dburl");
+
+            try {
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new DbException(e.getMessage());
+            }
+        }
+
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+
+            return props;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st == null)
+            return;
+
+        try {
+            st.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closePreparedStatement(PreparedStatement pst) {
+        if (pst == null)
+            return;
+
+        try {
+            pst.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs == null)
+            return;
+
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}
